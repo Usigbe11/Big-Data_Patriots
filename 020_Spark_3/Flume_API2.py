828a450696129a925df98dd788f82cf820655879
Usigbe11 <62066523+Usigbe11@users.noreply.github.com>
2020-04-14 14:29:23 -0400

    Big Data Assignments

---
+++ 020_Spark_3/Flume_API2.py
@@ -0,0 +1,48 @@
+#from pyspark.streaming.kafka import KafkaUtils
+from pyspark.streaming import StreamingContext
+from pyspark import SparkContext, SparkConf
+from pyspark import sql
+from pyspark.sql import SQLContext
+from pyspark.sql import types
+import json
+import csv
+from json import loads
+#from flatten_json import flatten
+from time import sleep
+#import pandas as pd
+from pyspark.streaming.flume import FlumeUtils
+
+
+sc =SparkContext()
+ssc = StreamingContext(sc,5)
+flumeStream = FlumeUtils.createStream(ssc, "localhost", 6669)
+sqlContext = sql.SQLContext(sc)
+sleep(3)
+lines = flumeStream.map(lambda x: x[1])
+
+print("LINES START!!!")
+print("LINES START!!!")
+print("LINES START!!!")
+print("LINES START!!!")
+
+def transformer(rdd):
+    if not rdd.isEmpty():
+        rdd.lower()
+        rdd.split()
+        return rdd
+   
+   
+transform=lines.map(transformer)
+
+def build_df(rdd):
+    if not rdd.isEmpty():
+         global sqlc
+         return (len(rdd))
+         df=sqlc.createDataFrame(rdd,shema=['Time','Tweet','location'])
+         df.show()
+transform.foreachRDD(build_df)
+transform.pprint()
+ 
+ssc.start()
+ssc.awaitTermination()
+
